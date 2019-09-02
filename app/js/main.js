8aafb0280df6513705cf85c3b35c19ed983dbc50
arinabilous <bilousiryna6@gmail.com>
2019-09-03 02:59:34 +0300

    edit buttons

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,10 @@ $(document).ready(function() {
     $(".hide").show();
   });
 
+  var audios = $(".audio");
+  var Current = 0;
+  var arrayAhref = [];
+
   var audio = new Audio(); // Create a new item Audio
   $(".audio").click(function() {
     $(".audio").removeClass("play");
@@ -17,23 +21,22 @@ $(document).ready(function() {
     $(this).addClass("play");
 
     var textAudioName = document.getElementById("text-audio");
+
     textAudioName.textContent = this.childNodes[0].textContent;
+
+    Current = this.dataset.test - 1;
   });
 
-  var audios = $(".audio");
-  var Current = 0;
-  var arrayAhref = [];
-  // var arrayData = [];
   for (let i = 0; i < audios.length; i++) {
     arrayAhref.push(audios[i]);
-    // arrayData.push(audios[i].getAttribute('data-test'));
   }
 
-  document.getElementById("text-audio").innerHTML =
-    arrayAhref[Current].textContent;
+  // document.getElementById("text-audio").innerHTML = arrayAhref[0].textContent;
 
   $(".button.prev").click(function() {
-    if (Current == 0) {
+    if (Current == 1) {
+      Current = arrayAhref.length - 1;
+    } else if (Current == 0) {
       Current = arrayAhref.length - 1;
     } else {
       Current--;
@@ -48,7 +51,7 @@ $(document).ready(function() {
 
   $(".button.next").click(function() {
     if (Current == arrayAhref.length - 1) {
-      Current = 0;
+      Current = 1;
     } else {
       Current++;
     }
@@ -60,51 +63,145 @@ $(document).ready(function() {
     audio.play();
   });
 
+  var arraybutton = [];
+  var button = $(".list-music .button");
+  for (let i = 0; i < button.length; i++) {
+    arraybutton.push(button[i]);
+  }
+
+  arraybutton.forEach(function(elem) {
+    elem.addEventListener("click", function(event) {
+      var target = event.target;
+      if (target) {
+        var dataTest = $(".audio.play")[0].dataset.test;
+      }
+      switchtest(dataTest);
+    });
+  });
+
   arrayAhref.forEach(function(elem) {
     elem.addEventListener("click", function(event) {
       var target = event.target;
-
       if (target.classList.contains("play")) {
         var dataTest = target.getAttribute("data-test");
       } else {
         console.log("sory");
       }
-
-      switch (dataTest) {
-        case "1":
-          var roundButn = $(".round-butn");
-          roundButn.css({
-            transform: "rotate(150deg)"
-          });
-
-          break;
-        case "2":
-          var roundButn = $(".round-butn");
-          roundButn.css({
-            transform: "rotate(50deg)"
-          });
-          break;
-        case "3":
-          var roundButn = $(".round-butn");
-          roundButn.css({
-            transform: "rotate(30deg)"
-          });
-          break;
-        case "4":
-          var roundButn = $(".round-butn");
-          roundButn.css({
-            transform: "rotate(99deg)"
-          });
-          break;
-        case "5":
-          var roundButn = $(".round-butn");
-          roundButn.css({
-            transform: "rotate(130deg)"
-          });
-          break;
-        default:
-          console.log("Error");
-      }
+      switchtest(dataTest);
     });
   });
 });
+
+console.log($(".slider"));
+
+function switchtest(varEl) {
+  switch (varEl) {
+    case "1":
+      changeHu(
+        "rotate(35deg)",
+        "active",
+        "on",
+        "active",
+        "65%",
+        "calc(100% - 15px)",
+        "0",
+        "55%",
+        "65%",
+        "calc(100% - 15px)",
+        "0",
+        "55%"
+      );
+
+      // Osc1ActPanel.addClass("active");
+      // Osc1ButtonEffectsSin.addClass("on");
+
+      // Osc1roundButn1.css({
+      //   transform: "rotate(-35deg)"
+      // });
+      // Osc1roundButn4.css({
+      //   transform: "rotate(150deg)"
+      // });
+      // Osc1Numbers[0].innerText = "1";
+
+      // Osc1roundButn5.css({
+      //   transform: "rotate(220deg)"
+      // });
+      // Osc1roundButn6.css({
+      //   transform: "rotate(220deg)"
+      // });
+      // Osc1roundButn7.css({
+      //   transform: "rotate(220deg)"
+      // });
+
+      // Osc1roundButnDown1.css({
+      //   transform: "rotate(-50deg)"
+      // });
+      // Osc1roundButnDown2.css({
+      //   transform: "rotate(220deg)"
+      // });
+      // Osc1roundButnDown3.css({
+      //   transform: "rotate(140deg)"
+      // });
+      // Osc1roundButnDown4.css({
+      //   transform: "rotate(50deg)"
+      // });
+
+      break;
+    case "2":
+      changeHu();
+      break;
+    case "3":
+      changeHu();
+      break;
+    case "4":
+      changeHu();
+      break;
+    case "5":
+      changeHu();
+      break;
+    default:
+      console.log("Error");
+  }
+}
+
+function changeHu(var0,
+var1,
+var2,
+var3,
+var4,
+var5,
+var6,
+var7,
+var8,
+var9,
+var10,) {
+  // Remove
+  $(".round-butn").removeAttr("style");
+  // $(".act-panel").removeClass("active");
+  // $(".button-effects").removeClass("on");
+  // $(".switch").removeClass("active");
+  // $(".switch-vertical").removeClass("active");
+  // $(".slider").removeAttr("style");
+
+  // Add
+  var RoundButns = $(".round-butn");
+  for (let i = 0; i < 11; i++) {
+    var x = "var" + i;
+    var httt = $(".round-butn")[i].style.transform = x;
+    
+    console.log(var0);
+  }
+
+  // $(".round-butn")[0].style.transform = var1;
+  // $(".act-panel")[2].classList.add(var2);
+  // $(".button-effects")[0].classList.add(var3);
+  // $(".switch")[0].classList.add(var4);
+  // $(".slider")[0].style.top = var5;
+  // $(".slider")[1].style.top = var6;
+  // $(".slider")[2].style.top = var7;
+  // $(".slider")[3].style.top = var8;
+  // $(".slider")[4].style.top = var9;
+  // $(".slider")[5].style.top = var10;
+  // $(".slider")[6].style.top = var11;
+  // $(".slider")[7].style.top = var12;
+}
