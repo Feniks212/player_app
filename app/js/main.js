fddd970a0892a88f668b9021dcaf1bb65f0ba777
Vladimir <v.fedchenko212@gmail.com>
2019-09-12 11:02:22 +0300

    new scripts random save 12.09.2019 11:02

--- app/js/main.js
+++ app/js/main.js
@@ -31,33 +31,33 @@ objectActPanels.secondRow = $(".second-row .act-panel");
 objectActPanels.lastRow = $(".last-row .act-panel");
 
 $(document).ready(function() {
-  let myTimer = setInterval(loopTime, 7000);
-  $(".overlapping").fadeOut();
+  // let myTimer = setInterval(loopTime, 7000);
+  // $(".overlapping").fadeOut();
 
-  let popup = document.querySelector(".open-popup");
-  popup.addEventListener("click", function(event) {
-    let target = event.target;
-    if (target) {
-      $(".overlapping")[0].style.opacity = "1";
-      $(".overlapping").fadeIn(500);
-      setTimeout(function() {
-        $(".overlapping").fadeOut(500);
-        $(".overlapping")[0].style.opacity = "0";
-      }, 3000);
-      clearInterval(myTimer);
-      myTimer = setInterval(loopTime, 11000);
-    }
-  });
-  function loopTime() {
-    $(".overlapping")[0].style.opacity = "1";
-    $(".overlapping").fadeIn(500);
-    setTimeout(() => {
-      $(".overlapping").fadeOut(500);
-      $(".overlapping")[0].style.opacity = "0";
-    }, 3000);
-  }
-  clearInterval(myTimer);
-  myTimer = setInterval(loopTime, 11000);
+  // let popup = document.querySelector(".open-popup");
+  // popup.addEventListener("click", function(event) {
+  //   let target = event.target;
+  //   if (target) {
+  //     $(".overlapping")[0].style.opacity = "1";
+  //     $(".overlapping").fadeIn(500);
+  //     setTimeout(function() {
+  //       $(".overlapping").fadeOut(500);
+  //       $(".overlapping")[0].style.opacity = "0";
+  //     }, 3000);
+  //     clearInterval(myTimer);
+  //     myTimer = setInterval(loopTime, 11000);
+  //   }
+  // });
+  // function loopTime() {
+  //   $(".overlapping")[0].style.opacity = "1";
+  //   $(".overlapping").fadeIn(500);
+  //   setTimeout(() => {
+  //     $(".overlapping").fadeOut(500);
+  //     $(".overlapping")[0].style.opacity = "0";
+  //   }, 3000);
+  // }
+  // clearInterval(myTimer);
+  // myTimer = setInterval(loopTime, 11000);
 
   // function to open a block with a list of audio
   $(".hide").click(function() {
@@ -160,12 +160,12 @@ $(document).ready(function() {
       }
     });
   });
-
   //iterate over all the elements from the "arrayAudio"
   arrayAudio.forEach(function(elem) {
     //check which button is pressed
     elem.addEventListener("click", function(event) {
       let target = event.target;
+
       if (target.classList.contains("play")) {
         // find out which element the class belongs to "play" and say what attribute it has
         let dataAudio = target.getAttribute("data-audio");
@@ -180,144 +180,373 @@ $(document).ready(function() {
           }
         });
         switchFunction(dataAudio);
+        console.log(dataAudio);
       } else {
         console.log("sory");
       }
     });
   });
+  const individuallyObj = new Object();
+  for (let i = 1; i < arrayAudio.length; i++) {
+    individuallyObj[i] = {
+      actPanels: actPanels,
+      buttonEffects: buttonEffects,
+      changeButtonFilters: changeButtonFilters,
+      roundButns: roundButns,
+      sliders: sliders,
+      switchVertical: switchVertical,
+      switchs: switchs
+    };
+  }
+
+  const settingRoundButns = new Object();
+  const settingActPanels = new Object();
+  const settingbuttonEffects = new Object();
+  const settingchangeButtonFilters = new Object();
+  const settingsliders = new Object();
+  const settingswitchVertical = new Object();
+  const settingswitchs = new Object();
+
+  Object.keys(individuallyObj).forEach(element => {
+    settingRoundButns[element] = new Array();
+    settingActPanels[element] = new Array();
+    settingbuttonEffects[element] = new Array();
+    settingchangeButtonFilters[element] = new Array();
+    settingsliders[element] = new Array();
+    settingswitchVertical[element] = new Array();
+    settingswitchs[element] = new Array();
+
+    //roundButns
+    for (let i = 0; i < individuallyObj[element].roundButns.length; i++) {
+      let htmlelement = individuallyObj[element].roundButns[i];
+      if (htmlelement.classList.contains("round-butn")) {
+        let rand = randomFunction(-150, 150); //random element "round-butn" MIN and MAX
+        let addToArray = `rotate(${rand}deg)`; //roundButns[i].style.transform =
+        settingRoundButns[element].push(addToArray);
+      } else {
+        console.log("error");
+      }
+    }
+
+    let classNameAct = "active";
+    let classNameOn = "on";
+
+    //ActPanels
+    let newArrayForAct = [];
+    individualSetting(
+      objectActPanels,
+      settingActPanels[element],
+      classNameAct,
+      newArrayForAct
+    );
+    settingActPanels[element] = [];
+    settingActPanels[element].push(newArrayForAct);
+
+    //buttonEffects
+    let newArraybuttonEffects = [];
+    individualSetting(
+      objectbuttonEffects,
+      settingbuttonEffects[element],
+      classNameOn,
+      newArraybuttonEffects
+    );
+    settingbuttonEffects[element] = [];
+    settingbuttonEffects[element].push(newArraybuttonEffects);
+
+    //ButtonFilters
+    let newArrayButtonFilters = [];
+    individualSettingSmall(
+      individuallyObj[element].changeButtonFilters,
+      settingchangeButtonFilters[element],
+      classNameOn,
+      newArrayButtonFilters
+    );
+    settingchangeButtonFilters[element] = [];
+    settingchangeButtonFilters[element].push(newArrayButtonFilters);
+
+    //switchs
+    let newArrayswitchs = [];
+    individualSettingSmall(
+      individuallyObj[element].switchs,
+      settingswitchs[element],
+      classNameAct,
+      newArrayswitchs
+    );
+    settingswitchs[element] = [];
+    settingswitchs[element].push(newArrayswitchs);
+
+    //switchsVertical
+    let newArrayswitchsVertical = [];
+    individualSettingSmall(
+      individuallyObj[element].switchVertical,
+      settingswitchVertical[element],
+      classNameAct,
+      newArrayswitchsVertical
+    );
+    settingswitchVertical[element] = [];
+    settingswitchVertical[element].push(newArrayswitchsVertical);
+
+    //Sliders
+    for (let i = 0; i < individuallyObj[element].sliders.length; i++) {
+      let htmlelement = individuallyObj[element].sliders[i];
+      if (htmlelement.classList.contains("slider")) {
+        let rand = randomFunction(0, 76); //random element "sliders" MIN and MAX
+        let addToArray = `${rand}%`; //roundButns[i].style.transform =
+
+        settingsliders[element].push(addToArray);
+      } else {
+        console.log("error");
+      }
+    }
+
+    console.log("end");
+  });
+  
+  console.log(settingswitchVertical);
+
+  function individualSetting(objects, arrays, classes, newArray) {
+    let obj = Object.values(objects);
+    for (let i = 0; i < obj.length; i++) {
+      let objelement = obj[i];
+      arrays.push(objelement);
+    }
+    arrays.forEach(element => {
+      let arrayforEach = [];
+      for (let i = 0; i < element.length; i++) {
+        element[i] = "";
+        const ros = element[i];
+        arrayforEach.push(ros);
+      }
+      let quantity = arrayforEach.length;
+      arrayforEach[randomFunction(0, quantity)] = `${classes}`;
+
+      for (let j = 0; j < arrayforEach.length; j++) {
+        const el = arrayforEach[j];
+        newArray.push(el);
+      }
+    });
+  }
+
+  function individualSettingSmall(objects, arrays, classes, newArray) {
+    for (let i = 0; i < objects.length; i++) {
+      let htmlelement = objects[i];
+      arrays.push(htmlelement);
+    }
+    for (let i = 0; i < arrays.length; i++) {
+      arrays[i] = "";
+      const ros = arrays[i];
+      newArray.push(ros);
+    }
+    let quantity = newArray.length;
+    newArray[randomFunction(0, quantity)] = `${classes}`;
+  }
+
+  // function switch, which sets the properties for each audio
+  function switchFunction(dateElement) {
+    switch (dateElement) {
+      case "1": // if the audion and video has a date attribute of "1"
+        changeRoundButn(settingRoundButns[1]);
+        changeActPanel(settingActPanels[1][0]);
+        randombuttonEffects(settingbuttonEffects[1][0]);
+        changeSliders(settingsliders[1]);
+        changeButtonFilter(settingchangeButtonFilters[1][0]);
+        changeSwitch(settingswitchs[1][0]);
+        changeSwitchVertical(settingswitchVertical[1][0]);
+        break;
+      case "2": // if the audion and video has a date attribute of "2"
+        changeRoundButn(settingRoundButns[2]);
+        changeActPanel(settingActPanels[2][0]);
+        randombuttonEffects(settingbuttonEffects[2][0]);
+        changeSliders(settingsliders[2]);
+        changeButtonFilter(settingchangeButtonFilters[2][0]);
+        changeSwitch(settingswitchs[2][0]);
+        changeSwitchVertical(settingswitchVertical[2][0]);
+        break;
+      case "3": // if the audion and video has a date attribute of "3"
+        changeRoundButn(settingRoundButns[3]);
+        changeActPanel(settingActPanels[3][0]);
+        randombuttonEffects(settingbuttonEffects[3][0]);
+        changeSliders(settingsliders[3]);
+        changeButtonFilter(settingchangeButtonFilters[3][0]);
+        changeSwitch(settingswitchs[3][0]);
+        changeSwitchVertical(settingswitchVertical[3][0]);
+        break;
+      default:
+        console.log("Error");
+    }
+  }
+
+  // function for all elements "RoundButn" in HTML
+  function changeRoundButn(arrays) {
+    // Remove all attribute
+    roundButns.removeAttr("style");
+    // Add properties
+    for (let i = 0; i < roundButns.length; i++) {
+      roundButns[i].style.transform = arrays[i];
+    }
+    // mainСycle(roundButns, arrays); // function start "mainСycle"
+  }
+  function changeSliders(arrays) {
+    // Remove all attribute
+    sliders.removeAttr("style");
+    //Add properties
+    for (let i = 0; i < sliders.length; i++) {
+      sliders[i].style.top = arrays[i];
+    }
+
+    // let arraySliders = [];
+    // for (let i = 0; i < sliders.length; i++) {
+    //   const element = sliders[i];
+    //   arraySliders.push(element);
+    // }
+    // arraySliders.forEach(element => {
+    //   element.style.top = `${randomFunction(0, 76)}%`;
+    // });
+    // mainСycle(sliders, arguments); // function start "mainСycle"
+  }
+
+  function changeButtonFilter(arrays) {
+    // Remove all classes
+    changeButtonFilters.removeClass("on");
+    //Add properties
+
+    for (let i = 0; i < changeButtonFilters.length; i++) {
+      if (arrays[i] != "") {
+        changeButtonFilters[i].classList.add(arrays[i]);
+      } else {
+        changeButtonFilters[i].classList.remove("on");
+      }
+    }
+
+    // changeButtonFilters[
+    //   randomFunction(0, changeButtonFilters.length)
+    // ].classList.add("on");
+    // mainСycle(changeButtonFilters, arguments); // function start "mainСycle"
+  }
+
+  function changeSwitch(arrays) {
+    // Remove all classes
+    switchs.removeClass("active");
+    //Add properties
+    for (let i = 0; i < switchs.length; i++) {
+      if (arrays[i] != "") {
+        switchs[i].classList.add(arrays[i]);
+      } else {
+        switchs[i].classList.remove("active");
+      }
+    }
+    // switchs[randomFunction(0, switchs.length)].classList.add("active");
+    // mainСycle(switchs, arguments); // function start "mainСycle"
+  }
+
+  function changeSwitchVertical(arrays) {
+    // Remove all classes
+    switchVertical.removeClass("active");
+    //Add properties
+    for (let i = 0; i < switchVertical.length; i++) {
+      if (arrays[i] != "") {
+        switchVertical[i].classList.add(arrays[i]);
+      } else {
+        switchVertical[i].classList.remove("active");
+      }
+    }
+    // mainСycle(switchVertical, arguments); // function start "mainСycle"
+  }
+
+  //function to random element "round-butn"
+  function randomFunction(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  function randombuttonEffects(arrays) {
+    // Remove all classes
+    buttonEffects.removeClass("on");
+
+    //Add properties
+    for (let i = 0; i < buttonEffects.length; i++) {
+      if (arrays[i] != "") {
+        buttonEffects[i].classList.add(arrays[i]);
+      } else {
+        buttonEffects[i].classList.remove("on");
+      }
+    }
+    // let obj = Object.values(objectbuttonEffects);
+    // let className = "on";
+    // mainchange(obj, className);
+  }
+
+  function changeActPanel(arrays) {
+    // Remove all classes
+    actPanels.removeClass("active");
+
+    //Add properties
+    for (let i = 0; i < actPanels.length; i++) {
+      if (arrays[i] != "") {
+        actPanels[i].classList.add(arrays[i]);
+      } else {
+        actPanels[i].classList.remove("active");
+      }
+    }
+    // let obj = Object.values(objectActPanels);
+    // let className = "active";
+    // mainchange(obj, className);
+
+    // mainСycle(actPanels, arguments);
+  }
+  // let obj = Object.values(objectActPanels);
+  // let className = "active";
+  // mainchange(obj, className);
+
+  // function mainchange(object, classes) {
+  //   let array = [];
+  //   for (let i = 0; i < object.length; i++) {
+  //     let element = object[i];
+  //     array.push(element);
+  //   }
+  //   array.forEach(element => {
+  //     let arrayforEach = [];
+  //     for (let i = 0; i < element.length; i++) {
+  //       const ros = element[i];
+  //       ros.classList.remove(`${classes}`);
+  //       arrayforEach.push(ros);
+  //     }
+  //     let quantity = arrayforEach.length;
+  //     arrayforEach[randomFunction(0, quantity)].classList.add(`${classes}`);
+  //   });
+  // }
+
+  function mainСycle(elem, properties) {
+    for (let i = 0; i < elem.length; i++) {
+      if (elem[i].classList.contains("round-butn")) {
+        let rand = randomFunction(-150, 150); //random element "round-butn" MIN and MAX
+        let addtoarray = `rotate(${rand}deg)`; //roundButns[i].style.transform =
+        properties.push(addtoarray);
+        elem[i].style.transform = properties[i];
+      } else if (
+        elem[i].classList.contains("act-panel") ||
+        elem[i].classList.contains("switch") ||
+        elem[i].classList.contains("switch-vertical")
+      ) {
+        if (properties[i] != "") {
+          elem[i].classList.add(properties[i]);
+        } else {
+          elem[i].classList.remove("active");
+        }
+      } else if (
+        elem[i].classList.contains("button-effects") ||
+        elem[i].classList.contains("filters")
+      ) {
+        if (properties[i] != "") {
+          elem[i].classList.add(properties[i]);
+          // randombuttonEffects();
+        } else {
+          elem[i].classList.remove("on");
+        }
+      } else if (elem[i].classList.contains("slider")) {
+        elem[i].style.top = properties[i];
+      } else {
+        console.log("error");
+      }
+    }
+  }
 });
-
-// function switch, which sets the properties for each audio
-function switchFunction(dateElement) {
-  switch (dateElement) {
-    case "1": // if the audion and video has a date attribute of "1"
-      changeRoundButn();
-      changeActPanel();
-      randombuttonEffects();
-      changeSwitch();
-      changeSwitchVertical();
-      changeSliders();
-      changeButtonFilter();
-      break;
-    case "2": // if the audion and video has a date attribute of "2"
-      changeRoundButn();
-      changeActPanel();
-      randombuttonEffects();
-      changeSwitch();
-      changeSwitchVertical();
-      changeSliders();
-      changeButtonFilter();
-      break;
-    case "3": // if the audion and video has a date attribute of "3"
-      changeRoundButn();
-      changeActPanel();
-      randombuttonEffects();
-      changeSwitch();
-      changeSwitchVertical();
-      changeSliders();
-      changeButtonFilter();
-      break;
-    default:
-      console.log("Error");
-  }
-}
-
-// function for all elements "RoundButn" in HTML
-function changeRoundButn() {
-  // Remove all attribute
-  roundButns.removeAttr("style");
-  // Add properties
-  for (let i = 0; i < roundButns.length; i++) {
-    if (roundButns[i].classList.contains("round-butn")) {
-      let rand = randomFunction(-150, 150); //random element "round-butn" MIN and MAX
-      roundButns[i].style.transform = `rotate(${rand}deg)`;
-    } else {
-      return false;
-    }
-  }
-}
-
-function changeButtonFilter() {
-  // Remove all classes
-  changeButtonFilters.removeClass("on");
-  //Add properties
-  changeButtonFilters[
-    randomFunction(0, changeButtonFilters.length)
-  ].classList.add("on");
-  // mainСycle(changeButtonFilters, arguments); // function start "mainСycle"
-}
-
-function changeSwitch() {
-  // Remove all classes
-  switchs.removeClass("active");
-  //Add properties
-  switchs[randomFunction(0, switchs.length)].classList.add("active");
-  // mainСycle(switchs, arguments); // function start "mainСycle"
-}
-
-function changeSwitchVertical() {
-  // Remove all classes
-  switchVertical.removeClass("active");
-  //Add properties
-  switchVertical[randomFunction(0, switchVertical.length)].classList.add(
-    "active"
-  );
-  // mainСycle(switchVertical, arguments); // function start "mainСycle"
-}
-
-function changeSliders() {
-  // Remove all attribute
-  sliders.removeAttr("style");
-  //Add properties
-  let arraySliders = [];
-  for (let i = 0; i < sliders.length; i++) {
-    const element = sliders[i];
-    arraySliders.push(element);
-  }
-  arraySliders.forEach(element => {
-    element.style.top = `${randomFunction(0, 76)}%`;
-  });
-  // mainСycle(sliders, arguments); // function start "mainСycle"
-}
-//function to random element "round-butn"
-function randomFunction(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function randombuttonEffects() {
-  // Remove all classes
-  buttonEffects.removeClass("on");
-
-  //Add properties
-  let obj = Object.values(objectbuttonEffects);
-  let className = "on";
-  mainchange(obj, className);
-}
-
-function changeActPanel() {
-  // Remove all classes
-  actPanels.removeClass("active");
-
-  //Add properties
-  let obj = Object.values(objectActPanels);
-  let className = "active";
-  mainchange(obj, className);
-}
-
-function mainchange(object, classes) {
-  let array = [];
-  for (let i = 0; i < object.length; i++) {
-    let element = object[i];
-    array.push(element);
-  }
-  array.forEach(element => {
-    let arrayforEach = [];
-    for (let i = 0; i < element.length; i++) {
-      const ros = element[i];
-      ros.classList.remove(`${classes}`);
-      arrayforEach.push(ros);
-    }
-    let quantity = arrayforEach.length;
-    arrayforEach[randomFunction(0, quantity)].classList.add(`${classes}`);
-  });
-}
